aa7b3ca54fb707ae4a588a89b1706002d2f1fe6e
ihih2g <38056825+ihih2g@users.noreply.github.com>
2021-12-29 14:56:48 +0800

    Update diy-K2T.sh

--- diy-K2T.sh
+++ diy-K2T.sh
@@ -21,4 +21,4 @@ sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai
 
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
